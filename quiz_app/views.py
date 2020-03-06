040053876f6f3d6a9765062062ad342b49e55781
Joni <joni@regen.fi>
2020-03-06 15:04:54 +0200

    More updates to quiz game view

--- quiz_app/views.py
+++ quiz_app/views.py
@@ -10,5 +10,10 @@ def FrontPageView(request):
 def AboutView(request):
 	return render(request, 'about.html')
 
-def QuizView(request):
-	return render(request, 'quiz.html')
+def QuizView(request, question_id):
+	questions = get_object_or_404(Question, pk=question_id)
+	return render(request, 'quiz.html', {'question': question})
+
+#MORE VIEWS
+#----Game Categories
+#----ALl Quiz Tips
