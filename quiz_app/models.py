deab409ff7bed9dcc428c022d744d2f952541f52
Joni <joni@regen.fi>
2020-03-06 14:03:31 +0200

    Quiz App Database functionality
    -Questions and Quiz Tips
    -Added possibility to modify both from admin

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=400)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
+class QuizTip(models.Model):
+    tip_title = models.CharField(max_length=200)
+    tip_text = models.CharField(max_length=400)
+    def __str__(self):
+        return self.tip_title
