501e05652c527c459210d212311a5b220ba19eeb
RegenDev <development@regen.fi>
2020-03-09 14:19:48 +0200

    More quiz app testing

--- quiz_app/views.py
+++ quiz_app/views.py
@@ -13,8 +13,11 @@ def AboutView(request):
 	return render(request, 'about.html')
 
 def QuizView(request):
-	questions = Question.objects.all()
+	questions = Question.objects.all()[:1]
+	latest_question_list = Question.objects.order_by('question_id')[:1]
 	return render(request, 'quiz.html', locals())
+#============TEST VIEWS=================#
+
 
 #MORE VIEWS
 #----Game Categories
